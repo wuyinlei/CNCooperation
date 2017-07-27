f422a73498791969caafa7bee6f872ec2dd9dc14
wuyinlei <power1118wu@gmail.com>
2017-07-27 22:24:26 +0800

    朋友圈UI编写实现,含有朋友圈编辑界面,朋友圈显示列表界面UI,朋友圈item界面编辑

---
+++ app/src/main/java/com/cainiao/cncooperation/ui/activity/FriendCircleActivity.java
@@ -0,0 +1,13 @@
+package com.cainiao.cncooperation.ui.activity;
+
+import com.cainiao.cncooperation.R;
+import com.cainiao.common.base.BaseActivity;
+
+public class FriendCircleActivity extends BaseActivity {
+
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_friend_circle;
+    }
+}
---
+++ common/src/main/java/com/cainiao/common/widget/ExpandTextView.java
@@ -0,0 +1,24 @@
+package com.cainiao.common.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by wuyinlei on 2017/7/27.
+ */
+
+public class ExpandTextView extends LinearLayout {
+    public ExpandTextView(Context context) {
+        super(context);
+    }
+
+    public ExpandTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpandTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
