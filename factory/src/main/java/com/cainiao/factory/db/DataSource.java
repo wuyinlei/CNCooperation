d1dea432da25b7ec6d11a3d4309a03afd417a36a
wuyinlei <power1118wu@gmail.com>
2017-08-07 10:16:25 +0800

    单聊的简单逻辑走通,但是接收到的信息处理需要整理,开发ing

---
+++ factory/src/main/java/com/cainiao/factory/db/DataSource.java
@@ -0,0 +1,43 @@
+package com.cainiao.factory.db;
+
+import android.support.annotation.StringRes;
+
+/**
+ * Created by wuyinlei on 2017/8/6.
+ *
+ * @function 数据源
+ */
+
+public interface DataSource {
+
+    /**
+     * 同时包括了成功和失败的接口
+     *
+     * @param <T> 任意类型
+     */
+    public interface Callback<T> extends SuccessCallback<T>, FailedCallback<T> {
+
+    }
+
+    /**
+     * 只关注成功的接口
+     *
+     * @param <T>
+     */
+    public interface SuccessCallback<T> {
+        //数据加载成功  网络请求成功
+        void onDataLoaded(T data);
+    }
+
+    /**
+     * 只关注失败的接口
+     *
+     * @param <T>
+     */
+    public interface FailedCallback<T> {
+        //数据加载失败  网络请求失败
+        void onDataNotAvaiable(@StringRes int res);
+    }
+
+    void dispose();
+}
--- factory/src/main/java/com/cainiao/factory/helper/MessageHelper.java
+++ factory/src/main/java/com/cainiao/factory/helper/MessageHelper.java
@@ -1,8 +1,23 @@
 package com.cainiao.factory.helper;
 
+import com.cainiao.factory.Factory;
+
+import io.rong.imlib.model.Message;
+
 /**
  * Created by wuyinlei on 2017/8/6.
  */
 
 public class MessageHelper {
+
+
+    public static void disposeMessage(Message message) {
+
+        Factory.runOnAsync(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
 }
